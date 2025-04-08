edd34da8dfbaa4613fabac4b3b8ac9c8008d448e
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-08 10:53:35 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q02/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q02/src/br/edu/principal/Principal.java
@@ -2,27 +2,30 @@ package br.edu.principal;
 
 import java.util.Scanner;
 
-public class Principal {
+	public class Principal {
+		public static void main(String[] args) {
+		        
+		        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		Double n1,n2,n3,mult;
-		
-		System.out.println("Digite o primeiro número: ");
-		n1 = sc.nextDouble();
-		
-		System.out.println("Digite o segundo número: ");
-		n2 = sc.nextDouble();
-				
-				System.out.println("Digite o terceiro numero: ");
-		n3 = sc.nextDouble();
-		
-		mult = n1 * n2 * n3 ;
-		
-		System.out.println("A multiplicação dos três número é: " + mult);
+		       
+		        int n1, n2, n3, n4, media;
 
-	}
+		       
+		        System.out.print("Digite o primeiro número: ");
+		        n1 = scanner.nextInt();
+		        
+		        System.out.print("Digite o segundo número: ");
+		        n2 = scanner.nextInt();
+		        
+		        System.out.print("Digite o terceiro número: ");
+		        n3 = scanner.nextInt();
+		        
+		          
+		        media = (n1 + n2 + n3) / 3;
+		        
+		        System.out.println("A sua media: " + media);
 
-}
+		        
+		        scanner.close();
+		    }
+		}
