915ad03364b67206cb5f78b009d1cdf74dd1198c
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-08 10:43:10 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
@@ -1,8 +1,9 @@
 package br.edu.principal;
-	import java.util.Scanner;
-		public class Principal {
-    
-				public static void main(String[] args) {
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
 
         double salario, vendas;
 
