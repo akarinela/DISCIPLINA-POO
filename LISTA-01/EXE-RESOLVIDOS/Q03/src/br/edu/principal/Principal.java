917f86f827aff393d6d46b1eda245d8f46275a08
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-08 10:54:14 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q03/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q03/src/br/edu/principal/Principal.java
@@ -2,23 +2,36 @@ package br.edu.principal;
 
 import java.util.Scanner;
 
-public class Principal {
+	public class Principal {
+		public static void main(String[] args) {
+		        
+		        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-        Double n1,n2,div;
-		
-		System.out.println("Digite um número: ");
-		 n1 = sc.nextDouble();
-		 
-		 System.out.println("Digite outro número: ");
-		 n2 = sc.nextDouble();
-		 
-		 div = n1 / n2;
-		 
-		 System.out.println("A divisão do primeiro numero pelo segundo é: " + div);
-		
-	}
+		        int nota1, nota2, nota3, peso1, peso2, peso3, media;
 
-}
+		       System.out.print("Digite a primeira nota: ");
+		        nota1 = scanner.nextInt();
+		        
+		        System.out.print("Digite a segunda nota: ");
+		        nota2 = scanner.nextInt();
+		        
+		        System.out.print("Digite  terceira nota: ");
+		        nota3 = scanner.nextInt();
+		
+		        System.out.print("Digite o primeiro peso: ");
+		        peso1 = scanner.nextInt();
+		        
+		        System.out.print("Digite o segundo peso: ");
+		        peso2 = scanner.nextInt();
+		        
+		        System.out.print("Digite o terceiro peso: ");
+		        peso3 = scanner.nextInt();
+		
+		       media = (nota1*peso1+ nota2*peso2+ nota3*peso3)/(peso1+ peso2+ peso3);
+		        
+		
+		        System.out.println("Media ponderada é: " + media);
+ 
+		        scanner.close();
+		    }
+		}
