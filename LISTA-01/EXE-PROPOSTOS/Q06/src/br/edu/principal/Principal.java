35d44e4149994e57a841c357a1018ebedbed842d
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-08 10:41:16 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
@@ -1,5 +1,18 @@
 package br.edu.principal;
 
-public class Principal {
+import java.util.Scanner;
 
+	public class Principal {
+		public static void main(String[] args) {
+
+        double peso;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite seu peso (kg): ");
+        peso = sc.nextDouble();
+
+        double gramas = peso * 1000;
+
+        System.out.println("Seu peso em gramas é, " + gramas);
+    }
 }
