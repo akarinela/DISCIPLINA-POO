e8d07bd483230d630d750228f56f22f595e65525
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-08 10:49:22 -0300

    Update module-info.java

--- LISTA-01/EXE-PROPOSTOS/Q10/src/module-info.java
+++ LISTA-01/EXE-PROPOSTOS/Q10/src/module-info.java
@@ -1,17 +1,9 @@
-package br.edu.principal;
 
-import java.util.Scanner;
-public class Principal {
-		public static void main(String[] args) {
-
-        double lado;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Digite o lado do quadrado: ");
-        lado = sc.nextDouble();
-
-        double area = lado * lado;
-
-        System.out.println("A área do quadrado é, " + area);
-    }
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Q10 {
 }
