66dbb1bfef1661b89bc2fff220f6e984e148fe17
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-11 08:36:50 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q10/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q10/src/br/edu/principal/Principal.java
@@ -1,5 +1,15 @@
-package br.edu.principal;
+import java.util.Scanner;
 
-public class Principal {
+public class AreaCirculo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite o valor do raio do círculo: ");
+        double raio = scanner.nextDouble();
+
+        double area = 3.1415 * raio * raio;
+
+        System.out.printf("A área do círculo é: %.2f%n", area);
+
+    }
 }
