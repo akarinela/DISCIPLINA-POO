fba935809cd338dc5c2e1aa1e11df7498c13b479
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-11 08:20:59 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q06/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q06/src/br/edu/principal/Principal.java
@@ -1,5 +1,19 @@
-package br.edu.principal;
+import java.util.Scanner;
 
-public class Principal {
+public class Salario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite o salário base: ");
+        double sal = scanner.nextDouble();
+      
+        double grat = sal * 0.05;
+        double imp = sal * 0.07;
+
+        double salReceber = sal + grat - imp;
+
+        System.out.printf("Salário a receber: R$ %.2n", salReceber);
+
+        scanner.close();
+    }
 }
