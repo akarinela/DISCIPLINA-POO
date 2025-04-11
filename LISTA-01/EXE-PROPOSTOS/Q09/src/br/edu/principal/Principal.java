a9a0076d66e4f9336f7dc3490487599cb7e452bc
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-11 08:41:53 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q09/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q09/src/br/edu/principal/Principal.java
@@ -10,8 +10,10 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite a base maior do trapézio: ");
         baseMaior = sc.nextDouble();
+			
         System.out.print("Digite a base menor do trapézio: ");
         baseMenor = sc.nextDouble();
+			
         System.out.print("Digite a altura do trapézio: ");
         altura = sc.nextDouble();
 
