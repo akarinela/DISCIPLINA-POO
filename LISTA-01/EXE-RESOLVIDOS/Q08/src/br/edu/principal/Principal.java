4c320fd07e0060152053d5821c6c53f5ee4eebed
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-11 08:28:25 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q08/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q08/src/br/edu/principal/Principal.java
@@ -1,5 +1,21 @@
-package br.edu.principal;
+import java.util.Scanner;
 
-public class Principal {
+public class RendimentoDeposito {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite o valor do depósito: ");
+        double dep = scanner.nextDouble();
+
+        System.out.print("Digite a taxa de juros: ");
+        double taxa = scanner.nextDouble();
+
+        double rend = dep * taxa / 100;
+        double total = dep + rend;
+
+        System.out.printf("Rendimento:" + rend);
+        System.out.printf("Total após rendimento:" + total);
+
+        scanner.close();
+    }
 }
