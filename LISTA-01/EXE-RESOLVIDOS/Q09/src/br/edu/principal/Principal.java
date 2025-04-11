2697797fdcc1282e9bca7afca70e3cd0b6d83e14
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-11 08:35:31 -0300

    Update Principal.java

--- LISTA-01/EXE-RESOLVIDOS/Q09/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-RESOLVIDOS/Q09/src/br/edu/principal/Principal.java
@@ -1,5 +1,18 @@
-package br.edu.principal;
+import java.util.Scanner;
 
-public class Principal {
+public class AreaTriangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite o valor da base do triângulo: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Digite o valor da altura do triângulo: ");
+        double altura = scanner.nextDouble();
+
+        double area = (base * altura) / 2;
+
+        System.out.printf("A área do triângulo é: + area);
+
+    }
 }
